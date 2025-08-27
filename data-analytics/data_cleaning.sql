eccb4587d268966688f680712092101fbde2ecf0
Abhi-109 <abhimalnad9@gmail.com>
2025-08-27 16:17:33 +0530

    Updated Data Cleaning

--- data-analytics/data_cleaning.sql
+++ data-analytics/data_cleaning.sql
@@ -36,3 +36,55 @@ WHERE region IS NULL;
 
 UPDATE Orders SET region = 'Canada'
 WHERE region IS NULL;
+
+SELECT SUBSTRING_INDEX(product_name,',' ,1) AS product,
+	    TRIM(SUBSTRING_INDEX(product_name,',',-1)) AS product_features
+FROM Orders; -- product name column have features combine it
+
+
+ALTER TABLE Orders
+ADD COLUMN product_features VARCHAR(150) AFTER product_name,
+ADD COLUMN products VARCHAR(150) AFTER product_name;
+
+UPDATE Orders
+SET products=SUBSTRING_INDEX(product_name,',' ,1),
+    product_features=TRIM(SUBSTRING_INDEX(product_name,',',-1)) ;
+
+ALTER TABLE Orders
+DROP COLUMN product_name;
+
+ALTER TABLE Orders
+RENAME COLUMN products TO product_name;
+
+-- we had a column called product name which had both name of the product  and feature of  that product so we split it that column into two parts as product name and product feature 
+SELECT ROUND(((sales/quantity)-shipping_cost),3) AS price ,sales,quantity
+FROM Orders;
+
+SELECT ROUND((sales-profit),3) AS product_cost,sales,profit
+FROM Orders;
+
+ALTER TABLE Orders
+	ADD COLUMN price decimal(10,3) AFTER product_features,
+	ADD COLUMN product_cost decimal(10,3) AFTER sales;
+    
+UPDATE Orders
+	SET price=ROUND(((sales/quantity)-shipping_cost),3),
+    product_cost =ROUND((sales-profit),3);
+    
+SELECT (ship_date-order_date) 
+FROM Orders;
+
+ALTER TABLE Orders
+ADD COLUMN days_delayed INT;
+
+UPDATE Orders
+	SET days_delayed=ship_date-order_date;
+    
+SELECT ((sales-product_cost)/sales) * 100
+FROM Orders;
+
+ALTER TABLE Orders
+ADD COLUMN gross_margin DECIMAL(10,3);
+
+UPDATE Orders
+SET gross_margin =ROUND(((sales-product_cost)/sales) * 100,3)
